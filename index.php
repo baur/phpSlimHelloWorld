fe0dc89f592250fda3cc9c34264094fc13b4ac7a
baur114 <baurdotnet@gmail.com>
2018-12-31 23:50:07 +0600

    add about page

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-
+use Slim\Views\PhpRenderer;
 require 'vendor/autoload.php';
 
 $app = new Slim\App();
 
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("./templates");
+
 $app->get('/hello/{name}', function ($request, $response, $args) {
 	    return $response->getBody()->write("Hello, " . $args['name']);
 });
+
+$app->get('/about', function ($request, $response, $args) {
+	      return $this->renderer->render($response, "/about.phtml", $args);
+	        });
 
 $app->run();
